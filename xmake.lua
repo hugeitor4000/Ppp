eef7e63005bbaba22be5cea83660778cc0b52d34
hugeitor4000 <elurbanoaranjuez@gmail.com>
2026-05-19 23:01:01 +0200

    Add files via upload

---
+++ xmake.lua
@@ -0,0 +1,17 @@
+add_rules("mode.debug", "mode.release", "mode.releasedbg")
+
+add_repositories("liteldev-repo https://github.com/LiteLDev/xmake-repo.git")
+
+add_requires("levilamina@0.13.x")
+
+if not has_config("vs_runtime") then
+    set_runtimes("MD")
+end
+
+target("immortal-zombie")
+    add_rules("@levilamina/modlibrary")
+    add_files("src/**.cpp")
+    add_includedirs("src")
+    add_packages("levilamina")
+    set_languages("c++20")
+    set_exceptions("none")
